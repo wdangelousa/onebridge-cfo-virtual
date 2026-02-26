847c2264b3dd50eef9a5b53b3a113c56c206b24a
Walter D'Angelo <wdangelo81@gmail.com>
2026-02-26 15:52:10 -0500

    Fix: Standardize imports and remove importmap for clean build

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 
